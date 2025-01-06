7fe1493f819f492f4c5b205ce11370057f9939d4
chefdude99 <chefdude.1999@gmail.com>
2025-01-06 19:00:06 +0000

    Casi listo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["Dalila", "Akira", "Turron", "Liza"];
+  let action = [" se comió", " se orinó", " dañó", " rompió"];
+  let what = [" mi cuarto", " mi celular", " el carro"];
+  let when = [
+    " luego de llegar",
+    " mientras dormia",
+    " durante mi entrenamiento",
+    " cuando fui a almorzar"
+  ];
+  let rdm1 = Math.floor(Math.random() * who.length);
+  let rdm2 = Math.floor(Math.random() * action.length);
+  let rdm3 = Math.floor(Math.random() * what.length);
+  let rdm4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[rdm1] + action[rdm2] + what[rdm3] + when[rdm4];
 };
